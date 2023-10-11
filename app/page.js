a16be9bd1c8953c995f11b52e5b4fc108059a5c3
MPacres <marcanthonypacres@gmail.com>
2023-10-11 21:17:06 +0800

    message fix

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="./marc-profile/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
